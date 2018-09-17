345000c9b39f62687c0b2d46fb66299267c76d8c
Kamran Mackey <kamranm1200@gmail.com>
2018-09-17 16:39:59 -0600

    Slight file/folder restructuring.
    Moved the strings.json file into a dedicated assets folder as I'd like
    to expand JSON use for various things, one thing being the ability to
    rotate between activities after a certain amount of time has passed.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/util/purge.js
+++ src/commands/util/purge.js
@@ -20,7 +20,7 @@
  */
 const { Command } = require('discord-akairo');
 const pluralize = require('pluralize');
-const strings = require('../../strings.json');
+const strings = require('../../assets/json/strings.json');
 
 class PurgeCommand extends Command {
     constructor() {
