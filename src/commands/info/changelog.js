752497472eaba12046f46e79dbf041deae3985eb
Kamran Mackey <kamranm1200@gmail.com>
2019-02-17 02:45:36 -0700

    Oops...fix changelog.js's license information.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/info/changelog.js
+++ src/commands/info/changelog.js
@@ -1,20 +1,20 @@
 /**
- * changelog.js -- Fetches the most recent commits to bot.
+ * changelog.js -- Fetches the most recent commits to Erica.
  * 
  * Copyright (c) 2018-present Kamran Mackey.
  * 
- * bot is free software: you can redistribute it and/or modify
+ * Erica is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * bot is distributed in the hope that it will be useful,
+ * Erica is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with bot. If not, see <https://www.gnu.org/licenses/>.
+ * along with Erica. If not, see <https://www.gnu.org/licenses/>.
  */
 
 const { Command } = require('discord-akairo');
