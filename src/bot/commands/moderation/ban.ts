410a4b61c069426383326abb48cf7e24a802d98a
Kamran Mackey <kamranm1200@gmail.com>
2019-03-16 15:58:06 -0600

    Implemented !ban.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/bot/commands/moderation/ban.ts
+++ src/bot/commands/moderation/ban.ts
@@ -18,7 +18,7 @@
  * along with Ellie. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { GuildMember, Message, MessageEmbed } from 'discord.js';
+import { DiscordAPIError, GuildMember, Message, MessageEmbed } from 'discord.js';
 
 import { Command } from 'discord-akairo';
 
@@ -38,12 +38,16 @@ export default class BanCommand extends Command {
         },
         {
           id: 'days',
-          type: 'number',
+          type: 'integer',
+          match: 'option',
+          flag: ['--days', '-d'],
+          default: 7,
         },
         {
           id: 'reason',
           match: 'rest',
           type: 'string',
+          default: '',
         },
       ],
     });
@@ -59,8 +63,31 @@ export default class BanCommand extends Command {
       return message.channel.send('You did not give a reason as to why you\'d like to ban this member.');
     }
 
-    await member.ban({ days, reason }).then(() => {
+    await member.ban({ reason, days }).then(() => {
       const BAN_EMBED = new MessageEmbed();
+
+      BAN_EMBED.setTitle('Member banned.');
+      BAN_EMBED.setColor(member.displayHexColor);
+      BAN_EMBED.setThumbnail(member.user.displayAvatarURL());
+      BAN_EMBED.setDescription(
+        `**Guild**: ${member.guild.name}\n` +
+        `**Member**: ${member.user.tag}\n` +
+        `**Duration**: ${days} days\n` +
+        `**Reason**: ${reason}`,
+      );
+
+      message.channel.send(BAN_EMBED);
+
+    }).catch((err) => {
+
+      if (err.code === 50013) {
+        return message.channel.send('Sorry, it looks like I am missing the proper permissions ' +
+          `so I cannot ban **${member.user.tag}**. Please make sure I have the right permissions ` +
+          'and then try again. Also, make sure my role is at the top of the role list!');
+      }
+
+      this.client.logger.error(err);
+      return message.channel.send(`I was unable to ban member **${member.user.tag}**.`);
     });
   }
 }
