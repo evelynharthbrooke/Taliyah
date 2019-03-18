24b57a078c00dbf21c883d711add4efa0b32437f
Kamran Mackey <kamranm1200@gmail.com>
2019-03-17 19:09:23 -0600

    Added start of a weather command.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

---
+++ src/bot/commands/extra/weather.ts
@@ -0,0 +1,37 @@
+/**
+ * weather.ts -- Gets the forecast for a specified location.
+ *
+ * Copyright (c) 2019-present Kamran Mackey.
+ *
+ * Ellie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ellie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ellie. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Message, MessageEmbed } from 'discord.js';
+
+import { Command } from 'discord-akairo';
+
+export default class WeatherCommand extends Command {
+  public constructor() {
+    super('weather', {
+      aliases: ['weather', 'forecast'],
+      category: 'Extra',
+    });
+  }
+
+  public async exec(message: Message) {
+    const DARK_SKY_API_URL = 'https://api.darksky.net';
+    return message.channel.send('Not implemented yet.');
+  }
+
+}
