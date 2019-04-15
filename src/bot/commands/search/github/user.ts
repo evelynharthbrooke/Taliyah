9879a03e9be54e08e72128e187a851fdbd73b6d8
Kamran Mackey <kamranm1200@gmail.com>
2019-04-14 21:45:40 -0600

    Added repo count and join date to GitHub user.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/bot/commands/search/github/user.ts
+++ src/bot/commands/search/github/user.ts
@@ -21,6 +21,7 @@
 import { Command } from 'discord-akairo';
 import { Message, MessageEmbed } from 'discord.js';
 
+import moment from 'moment';
 import graphql from '@octokit/graphql';
 
 export default class GitHubUserCommand extends Command {
@@ -52,10 +53,14 @@ export default class GitHubUserCommand extends Command {
         bio
         company
         url
+        createdAt
         isEmployee
         avatarUrl
         status {
           message
+        }
+        repositories {
+          totalCount
         }
         following {
           totalCount
@@ -67,15 +72,18 @@ export default class GitHubUserCommand extends Command {
     }`;
 
     const { user: GH_USER } = await GITHUB_API(USER_QUERY); // The GitHub API request.
+
     const GH_USER_NAME = GH_USER.name ? GH_USER.name : GH_USER.login; // The name of the GitHub user.
     const GH_USER_AVATAR = GH_USER.avatarUrl; // The avatar belonging to the GitHub user.
     const GH_USER_URL = GH_USER.url; // The URL linking to the user's GitHub profile.
+    const GH_USER_JOIN_DATE = moment(GH_USER.createdAt).format('LL'); // The date of when the user joined GitHub.
     const GH_USER_BIOGRAPHY = GH_USER.bio ? GH_USER.bio : ''; // The biography listed on the user's GitHub profile.
     const GH_USER_FOLLOWING = GH_USER.following.totalCount; // The amount of users the user is following.
     const GH_USER_FOLLOWERS = GH_USER.followers.totalCount; // The amount of users who are following the user.
     const GH_USER_STATUS = GH_USER.status ? GH_USER.status.message : 'No status listed.'; // The status listed on the user's profile.
     const GH_USER_LOCATION = GH_USER.location ? GH_USER.location : 'No location listed.'; // The location of the user.
     const GH_USER_COMPANY = GH_USER.company ? GH_USER.company : 'No company listed.'; // The company listed on the user's profile.
+    const GH_USER_REPOSITORIES = GH_USER.repositories.totalCount; // The amount of repositories the user has created or forked.
     const GH_USER_EMPLOYEE = GH_USER.isEmployee ? 'Yes' : 'No'; // Whether or not the user is an employee.
 
     GITHUB_EMBED.setTitle(GH_USER_NAME); // Set the title of the embed to the GitHub user's name on file.
@@ -83,12 +91,16 @@ export default class GitHubUserCommand extends Command {
     GITHUB_EMBED.setURL(GH_USER_URL); // Set the URL of the embed to the GitHub user's profile URL.
     GITHUB_EMBED.setDescription( // Set the description of the embed to contain all of the GitHub user information.
       `${GH_USER_BIOGRAPHY}\n\n` +
+      '**__Basic Details__**:\n' +
       `**Status**: ${GH_USER_STATUS}\n` +
+      `**Joined**: ${GH_USER_JOIN_DATE}\n` +
+      `**Repositories**: ${GH_USER_REPOSITORIES}\n` +
       `**Location**: ${GH_USER_LOCATION}\n` +
       `**Followers**: ${GH_USER_FOLLOWERS}\n` +
       `**Following**: ${GH_USER_FOLLOWING}\n` +
-      `**Company**: ${GH_USER_COMPANY}\n` +
-      `**GitHub Employee?** ${GH_USER_EMPLOYEE}`,
+      `**Company**: ${GH_USER_COMPANY}\n\n` +
+      '**__Other Details__**:\n' +
+      `**Is GitHub Employee**: ${GH_USER_EMPLOYEE}`,
     );
 
     return message.channel.send(GITHUB_EMBED);
