3c5a9d45ace49379ca8107ff758f912474b30d02
Kamran Mackey <kamranm1200@gmail.com>
2019-04-15 12:36:04 -0600

    Added more account types to GitHub user.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/bot/commands/search/github/user.ts
+++ src/bot/commands/search/github/user.ts
@@ -42,9 +42,13 @@ export default class GitHubUserCommand extends Command {
   }
 
   public async exec(message: Message, { user }: { user: string }) {
-    const GITHUB_EMBED = new MessageEmbed(); // The GitHub user embed.
-    const GITHUB_TOKEN = this.client.config.github.token; // The GitHub API token.
+    // The GitHub user embed.
+    const GITHUB_EMBED = new MessageEmbed();
+    // The GitHub API token.
+    const GITHUB_TOKEN = this.client.config.github.token;
+    // Initialize the GraphQL API wrapper.
     const GITHUB_API = graphql.defaults({ headers: { Authorization: `bearer ${GITHUB_TOKEN}` } });
+    // Build the GitHub user API query.
     const USER_QUERY = `query {
       user(login: "${user}") {
         login
@@ -54,9 +58,12 @@ export default class GitHubUserCommand extends Command {
         company
         url
         createdAt
-        isEmployee
         avatarUrl
         websiteUrl
+        isBountyHunter
+        isCampusExpert
+        isDeveloperProgramMember
+        isEmployee
         status {
           message
         }
@@ -72,26 +79,50 @@ export default class GitHubUserCommand extends Command {
       }
     }`;
 
-    const { user: GH_USER } = await GITHUB_API(USER_QUERY); // The GitHub API request.
+    // The GitHub API request.
+    const { user: GH_USER } = await GITHUB_API(USER_QUERY);
 
-    const GH_USER_NAME = GH_USER.name ? GH_USER.name : GH_USER.login; // The name of the GitHub user.
-    const GH_USER_AVATAR = GH_USER.avatarUrl; // The avatar belonging to the GitHub user.
-    const GH_USER_URL = GH_USER.url; // The URL linking to the user's GitHub profile.
-    const GH_USER_JOIN_DATE = moment(GH_USER.createdAt).format('LL'); // The date of when the user joined GitHub.
-    const GH_USER_BIOGRAPHY = GH_USER.bio ? GH_USER.bio : ''; // The biography listed on the user's GitHub profile.
-    const GH_USER_FOLLOWING = GH_USER.following.totalCount; // The amount of users the user is following.
-    const GH_USER_FOLLOWERS = GH_USER.followers.totalCount; // The amount of users who are following the user.
-    const GH_USER_WEBSITE = GH_USER.websiteUrl ? GH_USER.websiteUrl : 'No website listed.'; // The site listed on the user's profile.
-    const GH_USER_STATUS = GH_USER.status ? GH_USER.status.message : 'No status listed.'; // The status listed on the user's profile.
-    const GH_USER_LOCATION = GH_USER.location ? GH_USER.location : 'No location listed.'; // The location of the user.
-    const GH_USER_COMPANY = GH_USER.company ? GH_USER.company : 'No company listed.'; // The company listed on the user's profile.
-    const GH_USER_REPOSITORIES = GH_USER.repositories.totalCount; // The amount of repositories the user has created or forked.
-    const GH_USER_EMPLOYEE = GH_USER.isEmployee ? 'Yes' : 'No'; // Whether or not the user is an employee.
+    // The name of the GitHub user.
+    const GH_USER_NAME = GH_USER.name ? GH_USER.name : GH_USER.login;
+    // The avatar belonging to the GitHub user.
+    const GH_USER_AVATAR = GH_USER.avatarUrl;
+    // The URL linking to the user's GitHub profile.
+    const GH_USER_URL = GH_USER.url;
+    // The date of when the user joined GitHub.
+    const GH_USER_JOIN_DATE = moment(GH_USER.createdAt).format('LL');
+    // The biography listed on the user's GitHub profile.
+    const GH_USER_BIOGRAPHY = GH_USER.bio ? GH_USER.bio : '';
+    // The amount of users the user is following.
+    const GH_USER_FOLLOWING = GH_USER.following.totalCount;
+    // The amount of users who are following the user.
+    const GH_USER_FOLLOWERS = GH_USER.followers.totalCount;
+    // The website listed on the user's profile.
+    const GH_USER_WEBSITE = GH_USER.websiteUrl ? GH_USER.websiteUrl : 'No website listed.';
+    // The status listed on the user's profile.
+    const GH_USER_STATUS = GH_USER.status ? GH_USER.status.message : 'No status listed.';
+    // The location of the user.
+    const GH_USER_LOCATION = GH_USER.location ? GH_USER.location : 'No location listed.';
+    // The company listed on the user's profile.
+    const GH_USER_COMPANY = GH_USER.company ? GH_USER.company : 'No company listed.';
+    // The amount of repositories the user has created or forked.
+    const GH_USER_REPOSITORIES = GH_USER.repositories.totalCount;
+    // Whether or not the user is a developer program member.
+    const GH_USER_DEV_PROGRAM_MEMBER = GH_USER.isDeveloperProgramMember ? 'Yes' : 'No';
+    // Whether or not this user is a participant in the GitHub Security Bug Bounty.
+    const GH_USER_BUG_BOUNTY = GH_USER.isBountyHunter ? 'Yes' : 'No';
+    // Whether or not this user is a participant in the GitHub Campus Experts Program.
+    const GH_USER_CAMPUS_EXPERT = GH_USER.isCampusExpert ? 'Yes' : 'No';
+    // Whether or not this user is a GitHub employee.
+    const GH_USER_EMPLOYEE = GH_USER.isEmployee ? 'Yes' : 'No';
 
-    GITHUB_EMBED.setTitle(GH_USER_NAME); // Set the title of the embed to the GitHub user's name on file.
-    GITHUB_EMBED.setThumbnail(GH_USER_AVATAR); // Set the thumbnail of the embed to the GitHub user's avatar.
-    GITHUB_EMBED.setURL(GH_USER_URL); // Set the URL of the embed to the GitHub user's profile URL.
-    GITHUB_EMBED.setDescription( // Set the description of the embed to contain all of the GitHub user information.
+    // Set the title of the embed to the GitHub user's name on file.
+    GITHUB_EMBED.setTitle(GH_USER_NAME);
+    // Set the thumbnail of the embed to the GitHub user's avatar.
+    GITHUB_EMBED.setThumbnail(GH_USER_AVATAR);
+    // Set the URL of the embed to the GitHub user's profile URL.
+    GITHUB_EMBED.setURL(GH_USER_URL);
+    // Set the description of the embed to contain all of the GitHub user information.
+    GITHUB_EMBED.setDescription(
       `${GH_USER_BIOGRAPHY}\n\n` +
       '**__Basic Details__**:\n' +
       `**Status**: ${GH_USER_STATUS}\n` +
@@ -103,7 +134,10 @@ export default class GitHubUserCommand extends Command {
       `**Website**: ${GH_USER_WEBSITE}\n` +
       `**Company**: ${GH_USER_COMPANY}\n\n` +
       '**__Other Details__**:\n' +
-      `**Is GitHub Employee**: ${GH_USER_EMPLOYEE}`);
+      `**Bounty Hunter**: ${GH_USER_BUG_BOUNTY}\n` +
+      `**Campus Expert**: ${GH_USER_CAMPUS_EXPERT}\n` +
+      `**Developer Program Member**: ${GH_USER_DEV_PROGRAM_MEMBER}` +
+      `**GitHub Employee**: ${GH_USER_EMPLOYEE}\n`);
     GITHUB_EMBED.setFooter('Powered by the GitHub GraphQL API.'); // Set the embed footer.
     GITHUB_EMBED.setTimestamp(); // Set the embed's timestamp.
 
