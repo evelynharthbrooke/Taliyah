6f9bb4b42a6cc5e1176b0a67e667198b542b2454
Kamran Mackey <kamranm1200@gmail.com>
2019-08-30 14:40:13 -0600

    Renamed constants in about.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/bot/commands/info/about.ts
+++ src/bot/commands/info/about.ts
@@ -18,12 +18,12 @@
  * along with Ellie. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Command, version as DiscordAkairoVersion } from 'discord-akairo';
-import { Message, MessageEmbed, version as DiscordVersion } from 'discord.js';
+import { Command, version as akairo } from 'discord-akairo';
+import { Message, MessageEmbed, version as discord } from 'discord.js';
 import { Util } from '../../utils/Util';
 import moment from 'moment';
 import pluralize from 'pluralize';
-import { version as tsVersion } from 'typescript';
+import { version as typescript } from 'typescript';
 
 import * as packageJSON from '../../../../package.json';
 
@@ -39,46 +39,46 @@ export default class AboutCommand extends Command {
   }
 
   public async exec(message: Message) {
-    const aboutEmbed = new MessageEmbed();
-    /** Basic Bot Information */
-    const botVersion = packageJSON.version;
-    const botCodename = packageJSON.codename;
-    const botOwnerID = this.client.ownerID as string;
-    const botOwner = this.client.users.get(botOwnerID)!.tag;
-    const botMemoryUsage = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
-    const botUptime = moment.duration(-this.client.uptime!, 'milliseconds').humanize(true);
-    const nodeVersion = Util.parseNodeVersion(process.version);
-    const v8Version = process.versions.v8;
-    /** Statistics */
-    const channelFilter = this.client.channels.filter(channel => channel.type !== 'category').size;
-    const channelCount = pluralize('channel', channelFilter, true);
-    const guildCount = pluralize('guild', this.client.guilds.size, true);
-    const userCount = pluralize('user', this.client.guilds.map(g => g.memberCount).reduce((f, l) => f + l), true);
+    const embed = new MessageEmbed();
+    // Bot Information
+    const version = packageJSON.version;
+    const codename = packageJSON.codename;
+    const ownerID = this.client.ownerID as string;
+    const owner = this.client.users.get(ownerID)!.tag;
+    const memory = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
+    const uptime = moment.duration(-this.client.uptime!, 'milliseconds').humanize(true);
+    const node = Util.parseNodeVersion(process.version);
+    const v8 = process.versions.v8;
+    // Statistics
+    const filter = this.client.channels.filter(channel => channel.type !== 'category').size;
+    const channels = pluralize('channel', filter, true);
+    const guilds = pluralize('guild', this.client.guilds.size, true);
+    const users = pluralize('user', this.client.guilds.map(g => g.memberCount).reduce((f, l) => f + l), true);
 
-    aboutEmbed.setTitle(`About ${this.client.user!.username}`);
-    aboutEmbed.setColor(0x00AE86);
-    aboutEmbed.setThumbnail(this.client.user!.displayAvatarURL({ format: 'png', size: 1024 }));
-    aboutEmbed.setDescription(
+    embed.setTitle(`About ${this.client.user!.username}`);
+    embed.setColor(0x00AE86);
+    embed.setThumbnail(this.client.user!.displayAvatarURL({ format: 'png', size: 1024 }));
+    embed.setDescription(
       'Information about Ellie, such as her uptime, used libraries, etc. You can view '
       + 'her source code on GitHub [here](https://github.com/KamranMackey/Ellie/) and check her '
       + 'most recent commits by using **!changelog**. You can also view the help information for '
       + 'Ellie by using **!help**.\n\n'
       + '**__General__**:\n'
-      + `**Owner**: ${botOwner}\n`
-      + `**Started**: ${botUptime}\n`
-      + `**Guilds**: ${guildCount}\n`
-      + `**Channels**: ${channelCount}\n`
-      + `**Users**: ${userCount}\n`
-      + `**Version**: ${botVersion} ${botCodename}\n`
-      + `**Memory Usage**: ${botMemoryUsage} MB\n\n`
+      + `**Owner**: ${owner}\n`
+      + `**Started**: ${uptime}\n`
+      + `**Guilds**: ${guilds}\n`
+      + `**Channels**: ${channels}\n`
+      + `**Users**: ${users}\n`
+      + `**Version**: ${version} ${codename}\n`
+      + `**Memory Usage**: ${memory} MB\n\n`
       + '**__Dependencies__**:\n'
-      + `**[Node.js](https://nodejs.org)**: ${nodeVersion}\n`
-      + `**[V8](https://v8.dev)**: ${v8Version}\n`
-      + `**[TypeScript](https://www.typescriptlang.org)**: ${tsVersion.substring(0, 9)}\n`
-      + `**[Discord.js](https://github.com/discordjs/discord.js)**: ${DiscordVersion}\n`
-      + `**[Akairo](https://github.com/1Computer1/discord-akairo)**: ${DiscordAkairoVersion}\n`,
+      + `**[Node.js](https://nodejs.org)**: ${node}\n`
+      + `**[V8](https://v8.dev)**: ${v8}\n`
+      + `**[TypeScript](https://www.typescriptlang.org)**: ${typescript.substring(0, 9)}\n`
+      + `**[Discord.js](https://github.com/discordjs/discord.js)**: ${discord}\n`
+      + `**[Akairo](https://github.com/1Computer1/discord-akairo)**: ${akairo}\n`,
     );
 
-    return await message.channel.send(aboutEmbed);
+    return await message.channel.send(embed);
   }
 }
