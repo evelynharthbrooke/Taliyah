8be089c0a1af3ac9d539a36a8fb4739df216a810
Kamran Mackey <kamranm1200@gmail.com>
2019-12-26 12:42:18 -0700

    Added more information to the guild command.
    Now its pretty much complete.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/info/guild.rs
+++ src/commands/info/guild.rs
@@ -4,7 +4,7 @@ use serenity::client::Context;
 use serenity::framework::standard::macros::command;
 use serenity::framework::standard::{CommandError, CommandResult};
 use serenity::model::channel::ChannelType::{Category, Text, Voice};
-use serenity::model::guild::ExplicitContentFilter;
+use serenity::model::guild::{ExplicitContentFilter, VerificationLevel};
 use serenity::model::prelude::Message;
 
 #[command]
@@ -16,14 +16,18 @@ pub fn guild(ctx: &mut Context, msg: &Message) -> CommandResult {
     let cache = &ctx.cache;
     let guild_id = msg.guild_id.unwrap();
     let cached_guild = cache.read().guild(guild_id).unwrap();
+    
     let guild = cached_guild.read();
-
     let guild_name = &guild.name;
     let guild_owner = guild.member(&ctx, guild.owner_id).unwrap().user.read().tag();
     let guild_channels = guild.channels.iter().filter(|(_, c)| c.read().kind != Category).collect::<Vec<_>>().len();
     let guild_channels_text = guild.channels.iter().filter(|(_, c)| c.read().kind == Text).collect::<Vec<_>>().len();
     let guild_channels_voice = guild.channels.iter().filter(|(_, c)| c.read().kind == Voice).collect::<Vec<_>>().len();
-    let guild_id = guild.id;
+    let guild_creation_date = guild_id.created_at().format("%B %e, %Y - %I:%M %p");
+    let guild_emojis = guild.emojis.len();
+    let guild_emojis_normal = guild.emojis.iter().filter(|(_, e)| e.animated == false).collect::<Vec<_>>().len();
+    let guild_emojis_animated = guild.emojis.iter().filter(|(_, e)| e.animated == true).collect::<Vec<_>>().len();
+    let guild_presences = guild.presences.len();
     let guild_members = guild.member_count;
     let guild_icon = guild.icon_url().unwrap();
 
@@ -53,10 +57,26 @@ pub fn guild(ctx: &mut Context, msg: &Message) -> CommandResult {
         &_ => guild.region.to_string(),
     };
 
-    let guild_roles = guild.roles.iter().filter(|&(_, r)| &r.id != guild.id.as_u64()).map(|(_, r)| &r.name).join(" / ");
-    let guild_role_count = guild.roles.iter().filter(|&(_, r)| &r.id != guild.id.as_u64()).collect::<Vec<_>>().len();
-    let guild_presences = guild.presences.len();
-    let guild_creation_date = guild_id.created_at().format("%B %e, %Y - %I:%M %p");
+    let guild_boosts = guild.premium_subscription_count;
+    let guild_boost_tier = match guild.premium_tier.num() {
+        0 => "No current tier (not boosted)".to_string(),
+        1 => "Level 1 (5+ boosts)".to_string(),
+        2 => "Level 2 (15+ boosts)".to_string(),
+        3 => "Level 3 (30+ boosts)".to_string(),
+        _ => "".to_string()
+    };
+
+    let guild_roles = guild.roles.iter().filter(|&(_, r)| &r.id != guild_id.as_u64()).map(|(_, r)| &r.name).join(" / ");
+    let guild_role_count = guild.roles.iter().filter(|&(_, r)| &r.id != guild_id.as_u64()).collect::<Vec<_>>().len();
+
+    let guild_verification_level = match guild.verification_level {
+        VerificationLevel::None => "None - Unrestricted".to_string(),
+        VerificationLevel::Low => "Low - Must have a verified email.".to_string(),
+        VerificationLevel::Medium => "Medium - Registered on Discord for 5+ minutes.".to_string(),
+        VerificationLevel::High => "(╯°□°）╯︵ ┻━┻ - In the server for 10+ minutes.".to_string(),
+        VerificationLevel::Higher => "┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻) - Must have a verified phone number.".to_string(),
+        _ => "".to_string()
+    };
 
     let mut highest = None;
     for (role_id, _) in &guild.roles {
@@ -78,8 +98,7 @@ pub fn guild(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.channel_id
         .send_message(&ctx, move |m| {
             m.embed(move |e| {
-                e.title(format!("Information on guild {}", guild_name));
-                e.thumbnail(guild_icon);
+                e.author(|a| a.name(&guild_name).icon_url(guild_icon));
                 e.colour(highest_role_color);
                 e.description(format!("
                     **Name**: {}\n\
@@ -87,14 +106,20 @@ pub fn guild(ctx: &mut Context, msg: &Message) -> CommandResult {
                     **Members**: {}\n\
                     **Members Online**: {}\n\
                     **Channels**: {} ({} text, {} voice)\n\
+                    **Emojis**: {} ({} animated, {} static)\n\
                     **Region**: {}\n\
                     **Creation Date**: {}\n\
+                    **Verification Level**: {}\n\
                     **Explicit Content Filter**: {}\n\
+                    **Nitro Boosts**: {}\n\
+                    **Nitro Boost Level**: {}\n\
                     **Highest Role**: {}\n\
                     **Roles ({})**: {}\n",
                     guild_name, guild_owner, guild_members, guild_presences, guild_channels,
-                    guild_channels_text, guild_channels_voice, guild_region, guild_creation_date,
-                    guild_explicit_filter, highest_role_name, guild_role_count, guild_roles
+                    guild_channels_text, guild_channels_voice, guild_emojis, guild_emojis_animated, 
+                    guild_emojis_normal, guild_region, guild_creation_date, guild_verification_level, 
+                    guild_explicit_filter, guild_boosts, guild_boost_tier, highest_role_name, 
+                    guild_role_count, guild_roles
                 ))
             })
         })
