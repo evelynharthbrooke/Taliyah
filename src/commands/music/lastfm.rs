5bfe80df7dd89a76d1503a7d1472cdbbbaddf8a4
Kamran Mackey <kamranm1200@gmail.com>
2020-01-24 22:34:26 -0700

    lastfm: Improved username error handling.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/music/lastfm.rs
+++ src/commands/music/lastfm.rs
@@ -39,7 +39,7 @@ pub fn lastfm(ctx: &mut Context, message: &Message, mut args: Args) -> CommandRe
         user = match database::get_user_lastfm_username(&message.author.id) {
             Ok(l) => l,
             Err(e) => {
-                error!("Could not get lastfm username in database: {}", e);
+                error!("Could not get Last.fm username in database: {}", e);
                 match args.single::<String>() {
                     Ok(a) => a.to_string(),
                     Err(_) => {
@@ -47,8 +47,12 @@ pub fn lastfm(ctx: &mut Context, message: &Message, mut args: Args) -> CommandRe
                             .channel_id
                             .send_message(&ctx, |m| {
                                 m.embed(|e| {
-                                    e.title("Error: No Last.fm username was provided.");
-                                    e.description("You did not provide a Last.fm username. Please enter one and then try again.");
+                                    e.title("Error: No Last.fm username was found or provided.");
+                                    e.description(
+                                        "I could not find a Last.fm username pertaining to your user record, or \
+                                        you did not provide a Last.fm username as an argument. Please set a username \
+                                        via the profile command, or provide a Last.fm username as an argument.\
+                                        ");
                                     e.color(0x00FF_0000);
                                     e
                                 });
@@ -74,9 +78,7 @@ pub fn lastfm(ctx: &mut Context, message: &Message, mut args: Args) -> CommandRe
                         .send_message(&ctx, |m| {
                             m.embed(|e| {
                                 e.title("Error: Invalid Last.fm username provided.");
-                                e.description(
-                                    "You have entered an invalid username. Please provide a valid one and then try again.",
-                                );
+                                e.description("You have entered an invalid username. Please provide a valid one and then try again.");
                                 e.color(0x00FF_0000);
                                 e
                             });
@@ -97,7 +99,7 @@ pub fn lastfm(ctx: &mut Context, message: &Message, mut args: Args) -> CommandRe
                             });
                             m
                         })
-                        .map_or_else(|e| Err(CommandError(e.to_string())), |_| Ok(()))
+                        .map_or_else(|e| Err(CommandError(e.to_string())), |_| Ok(()));
                 }
             },
             _ => {
@@ -113,7 +115,7 @@ pub fn lastfm(ctx: &mut Context, message: &Message, mut args: Args) -> CommandRe
                         });
                         m
                     })
-                    .map_or_else(|e| Err(CommandError(e.to_string())), |_| Ok(()))
+                    .map_or_else(|e| Err(CommandError(e.to_string())), |_| Ok(()));
             }
         },
     };
@@ -132,12 +134,23 @@ pub fn lastfm(ctx: &mut Context, message: &Message, mut args: Args) -> CommandRe
     };
 
     let user_url = user_info.url;
-    
+
     let user_username = match database::get_user_display_name(&message.author.id) {
-        Ok(dn) => dn.to_string(),
-        Err(_) => user_info.username.to_string()
+        Ok(dn) => {
+            let lastfm_name = match database::get_user_lastfm_username(&message.author.id) {
+                Ok(l) => l,
+                Err(_) => user_info.username.to_string(),
+            };
+
+            if lastfm_name == user {
+                dn.to_string()
+            } else {
+                user_info.username.to_string()
+            }
+        }
+        Err(_) => user_info.username.to_string(),
     };
-    
+
     let user_registered = NaiveDateTime::from_timestamp(user_info.registered.friendly_date, 0).format("%B %e, %Y - %I:%M %p");
     let user_scrobbles = utilities::format_int(user_info.total_tracks.parse::<isize>().unwrap());
 
