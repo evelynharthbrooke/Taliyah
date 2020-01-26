a554b55489a461aa3ee1064ef545862794942a7d
Kamran Mackey <kamranm1200@gmail.com>
2020-01-25 17:46:14 -0700

    profile: ctx -> context.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/info/profile.rs
+++ src/commands/info/profile.rs
@@ -19,18 +19,18 @@ use rustfm::Client;
 #[usage = "<user> or <blank>"]
 #[sub_commands(set)]
 #[only_in("guilds")]
-pub fn profile(ctx: &mut Context, message: &Message, args: Args) -> CommandResult {
-    let cache = &ctx.cache;
+pub fn profile(context: &mut Context, message: &Message, args: Args) -> CommandResult {
+    let cache = &context.cache;
     let guild_id = message.guild_id.ok_or("Failed to get GuildID from Message.")?;
     let cached_guild = cache.read().guild(guild_id).ok_or("Unable to retrieve guild")?;
     let member = if message.mentions.is_empty() {
         if args.is_empty() {
-            message.member(&ctx).ok_or("Could not find member.")?
+            message.member(&context).ok_or("Could not find member.")?
         } else {
             (*(cached_guild.read().members_containing(args.rest(), false, true).first().ok_or("couldn't find member")?)).clone()
         }
     } else {
-        guild_id.member(&ctx, message.mentions.first().ok_or("Failed to get user mentioned.")?)?
+        guild_id.member(&context, message.mentions.first().ok_or("Failed to get user mentioned.")?)?
     };
 
     let user_name = member.user.read().tag().to_string();
@@ -73,7 +73,7 @@ pub fn profile(ctx: &mut Context, message: &Message, args: Args) -> CommandResul
         }
     };
 
-    message.channel_id.send_message(&ctx, |m| {
+    message.channel_id.send_message(&context, |m| {
         m.embed(|e| {
             e.author(|a| {
                 a.name(format!("{}'s profile", user_name));
@@ -97,7 +97,7 @@ pub fn profile(ctx: &mut Context, message: &Message, args: Args) -> CommandResul
 
 #[command]
 #[only_in("guilds")]
-pub fn set(ctx: &mut Context, message: &Message, mut arguments: Args) -> CommandResult {
+pub fn set(context: &mut Context, message: &Message, mut arguments: Args) -> CommandResult {
     let connection = match database::get_database() {
         Ok(connection) => connection,
         Err(_) => return Ok(()),
@@ -111,11 +111,11 @@ pub fn set(ctx: &mut Context, message: &Message, mut arguments: Args) -> Command
     match property.as_str() {
         "twitter" => {
             if value.is_empty() {
-                message.channel_id.say(&ctx, "You did not provide a Twitter username. Please provide one!")?;
+                message.channel_id.say(&context, "You did not provide a Twitter username. Please provide one!")?;
                 return Ok(());
             };
             let _ = connection.execute("UPDATE profile SET twitter = ?1 WHERE user_id = ?2;", &[&value, &user_id[..]]);
-            message.channel_id.say(&ctx, format!("Your Twitter username has been set to `{}`.", &value))?;
+            message.channel_id.say(&context, format!("Your Twitter username has been set to `{}`.", &value))?;
         }
         "lastfm" => {
             let api_key: String = env::var("LASTFM_KEY").expect("No API key detected");
@@ -126,7 +126,7 @@ pub fn set(ctx: &mut Context, message: &Message, mut arguments: Args) -> Command
                 Err(e) => match e {
                     Error::LastFMError(InvalidParameter(e)) => match e.message.as_str() {
                         "User not found" => {
-                            message.channel_id.send_message(&ctx, |m| {
+                            message.channel_id.send_message(&context, |m| {
                                 m.embed(|e| {
                                     e.title("Error: Invalid username provided.");
                                     e.description("You cannot use this as your profile's Last.fm username.");
@@ -142,31 +142,31 @@ pub fn set(ctx: &mut Context, message: &Message, mut arguments: Args) -> Command
             };
 
             if value.is_empty() {
-                message.channel_id.say(&ctx, "You did not provide your last.fm username. Please provide one!")?;
+                message.channel_id.say(&context, "You did not provide your last.fm username. Please provide one!")?;
                 return Ok(());
             };
             let _ = connection.execute("UPDATE profile SET lastfm = ?1 WHERE user_id = ?2;", &[&value, &user_id[..]]);
-            message.channel_id.say(&ctx, format!("Your lastfm username has been set to {}.", &value))?;
+            message.channel_id.say(&context, format!("Your lastfm username has been set to {}.", &value))?;
         }
         "steam" => {
             if value.is_empty() {
-                message.channel_id.say(&ctx, "You did not provide a Steam ID. Please provide one!")?;
+                message.channel_id.say(&context, "You did not provide a Steam ID. Please provide one!")?;
                 return Ok(());
             };
             let _ = connection.execute("UPDATE profile SET steam = ?1 WHERE user_id = ?2;", &[&value, &user_id[..]]);
-            message.channel_id.say(&ctx, format!("Your Steam ID has been set to `{}`.", &value))?;
+            message.channel_id.say(&context, format!("Your Steam ID has been set to `{}`.", &value))?;
         }
         "name" => {
             if value.is_empty() {
-                message.channel_id.say(&ctx, "You did not provide a name. Please provide one!")?;
+                message.channel_id.say(&context, "You did not provide a name. Please provide one!")?;
                 return Ok(());
             };
             let _ = connection.execute("UPDATE profile SET display_name = ?1 WHERE user_id = ?2;", &[&value, &user_id[..]]);
             let _ = connection.close().unwrap();
-            message.channel_id.say(&ctx, format!("Your name has been set to {}.", &value))?;
+            message.channel_id.say(&context, format!("Your name has been set to {}.", &value))?;
         }
         _ => {
-            message.channel_id.say(&ctx, "That is not a valid profile property.")?;
+            message.channel_id.say(&context, "That is not a valid profile property.")?;
         }
     }
 
