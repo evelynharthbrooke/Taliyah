6206f12e02ab6240ea12f804d401ea87d74a2353
Kamran Mackey <kamranm1200@gmail.com>
2020-01-31 02:53:53 -0700

    info(user): Use variable names for format.
    That way the string is basically forced to go onto another line.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/info/user.rs
+++ src/commands/info/user.rs
@@ -8,7 +8,7 @@ use serenity::framework::standard::macros::command;
 use serenity::framework::standard::Args;
 use serenity::framework::standard::CommandResult;
 use serenity::model::gateway::Activity;
-use serenity::model::gateway::ActivityType::{Listening, Playing, Watching, Streaming, Custom};
+use serenity::model::gateway::ActivityType::{Custom, Listening, Playing, Streaming, Watching};
 use serenity::model::guild::Role;
 use serenity::model::prelude::Message;
 use serenity::model::user::OnlineStatus;
@@ -49,44 +49,52 @@ pub fn user(context: &mut Context, message: &Message, args: Args) -> CommandResu
         let presence = guild.presences.get(&user.id).unwrap();
 
         activities = presence.activities.iter().filter(|a| a.kind != Custom).map(|a: &Activity| {
-                let mut activity_name = a.name.as_str();
-                let activity_kind = match a.kind {
-                    Listening => {
-                        if activity_name == "Spotify" {
-                            let song = a.details.as_ref().unwrap();
-                            let artist = a.state.as_ref().unwrap().replace(";", " & ");
-                            let album = a.assets.as_ref().unwrap().large_text.as_ref().unwrap();
-                            let str_ = format!("track:{} artist:{} album:{}", song, artist.replace("&", "AND"), album.replace("&", "%26"));
-                            let track_search = spotify().search_track(str_.as_str(), 1, 0, None);
-                            let track_result = &track_search.unwrap();
-                            let results = &track_result.tracks.items;
-                            let track = results.first().unwrap();
-                            let image = track.album.images.first().unwrap();
-                            let album_art = image.url.as_str();
-                            track_art.push_str(album_art);
-                            format!("listening to **{}** by **{}** on the album **{}** via", song, artist, album)
-                        } else {
-                            "listening to".to_owned()
-                        }
+            let mut activity_name = a.name.as_str();
+            let activity_kind = match a.kind {
+                Listening => {
+                    if activity_name == "Spotify" {
+                        let song = a.details.as_ref().unwrap();
+                        let artist = a.state.as_ref().unwrap().replace(";", " & ");
+                        let album = a.assets.as_ref().unwrap().large_text.as_ref().unwrap();
+                            
+                        let sp_search_string = format!(
+                            "track: {track} artist: {artists} album: {album}",
+                            track = song,
+                            artists = artist.replace("&", "AND"),
+                            album = album.replace("&", "%26")
+                        );
+                            
+                        let sp_track_search = spotify().search_track(sp_search_string.as_str(), 1, 0, None);
+                        let sp_track_result = &sp_track_search.unwrap();
+                        let results = &sp_track_result.tracks.items;
+                        let track = results.first().unwrap();
+                        let image = track.album.images.first().unwrap();
+                        let album_art = image.url.as_str();
+                            
+                        track_art.push_str(album_art);
+                            
+                        format!("listening to **{}** by **{}** on the album **{}** via", song, artist, album)
+                    } else {
+                        "listening to".to_owned()
                     }
-                    Playing => {
-                        if activity_name == "Visual Studio Code" {
-                            let file = a.details.as_ref().unwrap().replace("Editing ", "");
-                            let project = a.state.as_ref().unwrap().replace("Workspace: ", "");
-                            let app = a.assets.as_ref().unwrap().small_text.as_ref().unwrap();
-                            activity_name = app;
-                            format!("working on the file **{}** in the project **{}** with", file, project)
-                        } else {
-                            "playing".to_owned()
-                        }
+                }
+                Playing => {
+                    if activity_name == "Visual Studio Code" {
+                        let file = a.details.as_ref().unwrap().replace("Editing ", "");
+                        let project = a.state.as_ref().unwrap().replace("Workspace: ", "");
+                        let app = a.assets.as_ref().unwrap().small_text.as_ref().unwrap();
+                        activity_name = app;
+                        format!("working on the file **{}** in the project **{}** with", file, project)
+                    } else {
+                        "playing".to_owned()
                     }
-                    Watching => "watching".to_owned(),
-                    Streaming => "streaming on".to_owned(),
-                    _ => "".to_owned(),
-                };
-                format!("{} **{}**", activity_kind, activity_name)
-            })
-            .join(" and ");
+                }
+                Watching => "watching".to_owned(),
+                Streaming => "streaming on".to_owned(),
+                _ => "".to_owned(),
+            };
+            format!("{} **{}**", activity_kind, activity_name)
+        }).join(" and ");
 
         let currently_status: String = format!("{} is currently ", user.name);
 
