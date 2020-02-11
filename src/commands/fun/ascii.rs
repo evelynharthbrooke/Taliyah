3a1a4a5311b240c642879abfe8d770247c450d6d
Kamran Mackey <kamranm1200@gmail.com>
2020-02-11 07:58:39 -0700

    fun(ascii): Improved font check comment.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/fun/ascii.rs
+++ src/commands/fun/ascii.rs
@@ -55,10 +55,10 @@ pub fn ascii(context: &mut Context, message: &Message, arguments: Args) -> Comma
     let font_in_text = text.split_whitespace().next().unwrap_or("");
     let fonts_request = client.get(font_url).send()?.text()?;
 
-    // when checking for fonts, make sure we do an additional check for both the font
-    // in question & that there is a space. this will not work with zero width spaces
-    // due to the check we already have in place, and any spaces that are inserted 
-    // without any additional characters are already trimmed or removed by Discord.
+    // when checking for fonts, make sure a check is made, checking for both the font name
+    // in question & that there is a space. this will not work with zero width spaces due
+    // to the check that is already in place, and any spaces that are inserted without any
+    // additional characters are already trimmed or removed by Discord.
     let request = if fonts_request.split_whitespace().any(|x| x == font_in_text) && text.contains("\u{0020}") {
         // i don't know why this required a type annotation...but anyway
         // this took me way longer than was necessary to figure out.
