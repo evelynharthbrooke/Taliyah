bce6be7705b1d5f679206d271e1b09199df4ef0e
sonro <sonro@gmx.com>
2020-02-15 20:34:12 +0000

    Add accessor functions to config module
    Create getters for all the fields in `Config` referencing the value
    from the loaded CONFIG state. Set "RUST_LOG" env variable when the data
    is loaded from the configuration file.

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,70 @@
-use lazy_static::*;
+//! Provides functions for accessing configuration values.
+//! These are loaded from a toml file on first request during runtime.
+use lazy_static::lazy_static;
 use serde::Deserialize;
+use std::fs;
+use std::path::Path;
+
+const CONFIG_FILE: &str = "config.toml";
+
+macro_rules! config_str_convert {
+    ($field:ident) => {
+        Some(&CONFIG.$field.as_ref()?)
+    };
+}
+
+/// Try to get `bot_codename` from loaded config
+pub fn bot_codename() -> Option<&'static str> {
+    config_str_convert!(bot_codename)
+}
+
+/// Try to get `client_id` from loaded config
+pub fn client_id() -> Option<&'static str> {
+    config_str_convert!(client_id)
+}
+
+/// Try to get `client_secret` from loaded config
+pub fn client_secret() -> Option<&'static str> {
+    config_str_convert!(client_secret)
+}
+
+/// Try to get `darksky` from loaded config
+pub fn darksky() -> Option<&'static str> {
+    config_str_convert!(darksky)
+}
+
+/// Try to get `discord_token` from loaded config
+pub fn discord_token() -> Option<&'static str> {
+    config_str_convert!(discord_token)
+}
+
+/// Try to get `discord_prefix` from loaded config
+pub fn discord_prefix() -> Option<&'static str> {
+    config_str_convert!(discord_prefix)
+}
+
+/// Try to get `github_key` from loaded config
+pub fn github_key() -> Option<&'static str> {
+    config_str_convert!(github_key)
+}
+
+/// Try to get `google_key` from loaded config
+pub fn google_key() -> Option<&'static str> {
+    config_str_convert!(google_key)
+}
+
+/// Try to get `lastfm_key` from loaded config
+pub fn lastfm_key() -> Option<&'static str> {
+    config_str_convert!(lastfm_key)
+}
+
+/// Try to get `rust_log` from loaded config
+pub fn rust_log() -> Option<&'static str> {
+    config_str_convert!(rust_log)
+}
 
 lazy_static! {
-    static ref CONFIG: Config = Config::load_from_file("config.toml");
+    static ref CONFIG: Config = Config::load_from_file(CONFIG_FILE);
 }
 
 #[derive(Deserialize, Debug, PartialEq)]
@@ -22,8 +84,18 @@ struct Config {
 impl Config {
     fn load_from_file<P: AsRef<std::path::Path>>(path: P) -> Config {
         let error_msg = "Unable to read config.toml";
-        let data = std::fs::read_to_string(path).expect(error_msg);
-        toml::from_str(&data).expect(error_msg)
+        let data = fs::read_to_string(path).expect(error_msg);
+        let mut config: Config = toml::from_str(&data).expect(error_msg);
+
+        if config.bot_codename.is_none() {
+            config.bot_codename = Some("Ellie".to_owned());
+        }
+        // When specified in config file, set log level
+        if let Some(level) = &config.rust_log {
+            std::env::set_var("RUST_LOG", &level);
+        }
+
+        config
     }
 }
 
@@ -32,6 +104,7 @@ mod test {
     use super::*;
 
     const TEST_CONFIG_FILE_NAME: &str = "testing-config.toml";
+    const BACKUP_CONFIG_FILE_NAME: &str = "config.toml.bak";
     const CONFIG_TOML: &str = r#"
         bot_codename = "TestName"
         client_id = "123456789"
@@ -79,9 +152,9 @@ mod test {
     #[test]
     fn load_from_file() {
         let expected_config = generate_expected_config();
-        create_test_file();
+        create_test_file(TEST_CONFIG_FILE_NAME);
         let test_config = Config::load_from_file(TEST_CONFIG_FILE_NAME);
-        remove_test_file();
+        remove_test_file(TEST_CONFIG_FILE_NAME);
         assert_eq!(expected_config, test_config);
     }
 
@@ -91,11 +164,63 @@ mod test {
         Config::load_from_file("invalid.toml");
     }
 
-    fn create_test_file() {
-        std::fs::write(TEST_CONFIG_FILE_NAME, CONFIG_TOML).expect("Unable to create testing config file");
+    #[test]
+    fn getters() {
+        temp_move_config_file();
+        create_test_file(CONFIG_FILE);
+
+        let expected_config = Config::load_from_file(CONFIG_FILE);
+
+        check_field(expected_config.bot_codename, bot_codename());
+        check_field(expected_config.client_id, client_id());
+        check_field(expected_config.client_secret, client_secret());
+        check_field(expected_config.darksky, darksky());
+        check_field(expected_config.discord_token, discord_token());
+        check_field(expected_config.discord_prefix, discord_prefix());
+        check_field(expected_config.github_key, github_key());
+        check_field(expected_config.google_key, google_key());
+        check_field(expected_config.lastfm_key, lastfm_key());
+        check_field(expected_config.rust_log, rust_log());
+
+        remove_test_file(CONFIG_FILE);
+        move_back_config_file();
     }
 
-    fn remove_test_file() {
-        std::fs::remove_file(TEST_CONFIG_FILE_NAME).expect("Unable to remove testing config file");
+    fn check_field(field: Option<String>, res: Option<&'static str>) {
+        let equal = match field {
+            Some(data) => {
+                if let Some(res_data) = res {
+                    res_data == data
+                } else {
+                    false
+                }
+            }
+            None => res.is_none(),
+        };
+        if !equal {
+            remove_test_file(CONFIG_FILE);
+            move_back_config_file();
+            panic!("Getter does not match fields");
+        }
+    }
+
+    fn temp_move_config_file() {
+        if Path::new(CONFIG_FILE).exists() {
+            fs::rename(CONFIG_FILE, BACKUP_CONFIG_FILE_NAME).expect("Unable to backup config file before testing");
+        }
+    }
+
+    fn move_back_config_file() {
+        if Path::new(BACKUP_CONFIG_FILE_NAME).exists() {
+            fs::rename(BACKUP_CONFIG_FILE_NAME, CONFIG_FILE).expect("Unable to restore backup of config file");
+        }
+    }
+
+    fn create_test_file<P: AsRef<Path>>(test_file_name: P) {
+        fs::write(test_file_name, CONFIG_TOML).expect("Unable to create testing config file");
+    }
+
+    fn remove_test_file<P: AsRef<Path>>(test_file_name: P) {
+        fs::remove_file(test_file_name).expect("Unable to remove testing config file");
     }
 }
