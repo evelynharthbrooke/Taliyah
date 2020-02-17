9db29772cf2d0db59053ef727ce543f20020e16a
Kamran Mackey <kamranm1200@gmail.com>
2020-02-17 11:42:18 -0700

    tmdb(collection): Applied cargo fmt suggestions.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/search/tmdb/collection.rs
+++ src/commands/search/tmdb/collection.rs
@@ -92,27 +92,28 @@ pub fn collection(context: &mut Context, message: &Message, arguments: Args) -> 
     let collection_url = format!("https://www.themoviedb.org/collection/{}", collection_id);
     let collection_overview = collection_result.overview;
     let collection_parts = collection_result.parts;
-    let mut collection_fields = Vec::with_capacity(collection_parts.len());
+    let mut collection_part_fields = Vec::with_capacity(collection_parts.len());
 
     for part in &collection_parts {
-        // This probably isn't the best implementation for getting a collection 
+        // This probably isn't the best implementation for getting a collection
         // movie's release date, because everytime a collection entry is processed,
         // its gonna make a request to The Movie Database, using additional requests
-        // in the process. While the TMDb API doesn't have rate limits, this might 
-        // become a bit network I/O intensive if there are a lot of movies in a given 
+        // in the process. While the TMDb API doesn't have rate limits, this might
+        // become a bit network I/O intensive if there are a lot of movies in a given
         // collection.
         let part_id = part.id;
+        let part_title = &part.title;
+        let part_release_date = part.release_date.format("%B %-e, %Y");
+        let part_summary = &part.overview;
         let movie_endpoint = format!("https://api.themoviedb.org/3/movie/{}", part_id);
         let movie_response = client.get(&movie_endpoint).query(&[("api_key", &api_key)]).send()?;
         let movie_result: Movie = movie_response.json()?;
-        let movie_status = movie_result.status;
-
-        let release_status = match movie_status.as_str() {
+        let movie_status = match movie_result.status.as_str() {
             "Planned" | "In Production" | "Post Production" => "releasing on",
             _ => "released",
         };
 
-        collection_fields.push((format!("{} — {} {}", part.title, release_status, &part.release_date.format("%B %-e, %Y")), &part.overview, false))
+        collection_part_fields.push((format!("{} — {} {}", part_title, movie_status, part_release_date), part_summary, false))
     }
 
     message.channel_id.send_message(&context, |message| {
@@ -122,7 +123,7 @@ pub fn collection(context: &mut Context, message: &Message, arguments: Args) -> 
             embed.thumbnail(collection_poster);
             embed.color(0x0001_d277);
             embed.description(collection_overview);
-            embed.fields(collection_fields);
+            embed.fields(collection_part_fields);
             embed.footer(|footer| footer.text("Powered by the The Movie Database API."));
             embed.timestamp(&Utc::now())
         })
