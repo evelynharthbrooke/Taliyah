1e52c9714d75d8a9351289ce779fee7d1507c36a
Kamran Mackey <kamranm1200@gmail.com>
2020-12-13 01:50:43 -0700

    Removed is_new check.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/listeners/events/guild_create.rs
+++ src/listeners/events/guild_create.rs
@@ -3,7 +3,7 @@ use crate::{read_config, DatabasePool};
 use serenity::{client::Context, model::guild::Guild};
 use tracing::info;
 
-pub async fn guild_create(context: Context, guild: Guild, is_new: bool) {
+pub async fn guild_create(context: Context, guild: Guild, _is_new: bool) {
     let config = read_config("config.toml");
     let pool = context.data.read().await.get::<DatabasePool>().cloned().unwrap();
 
@@ -11,16 +11,13 @@ pub async fn guild_create(context: Context, guild: Guild, is_new: bool) {
     let guild_id = guild.id.0 as i64;
     let guild_prefix = config.bot.general.prefix;
 
-    if is_new {
-        info!("Adding guild {} to the database.", guild_name);
-        sqlx::query("INSERT INTO guild_info (guild_id, guild_name, guild_prefix) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING")
-            .bind(guild_id)
-            .bind(&guild_name)
-            .bind(guild_prefix)
-            .execute(&pool)
-            .await
-            .unwrap();
-    }
+    sqlx::query("INSERT INTO guild_info (guild_id, guild_name, guild_prefix) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING")
+        .bind(&guild_id)
+        .bind(&guild_name)
+        .bind(&guild_prefix)
+        .execute(&pool)
+        .await
+        .unwrap();
 
     info!("Guild {} recognized and loaded.", guild_name);
 }
