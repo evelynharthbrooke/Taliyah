1a83ca4acaef6ae67d781c6f6ee82a194eda1104
Kamran Mackey <kamranm1200@gmail.com>
2021-06-02 01:31:51 -0600

    xkcd: condense variable display.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/fun/xkcd.rs
+++ src/commands/fun/xkcd.rs
@@ -3,7 +3,7 @@ use serde::Deserialize;
 use serenity::{
     client::Context,
     framework::standard::{macros::command, Args, CommandResult},
-    model::{interactions::ButtonStyle, prelude::Message}
+    model::{channel::ReactionType::Unicode, interactions::ButtonStyle::Link, prelude::Message}
 };
 
 use crate::data::ReqwestContainer;
@@ -20,44 +20,41 @@ struct XkcdComic {
 #[command]
 pub async fn xkcd(context: &Context, message: &Message, mut arguments: Args) -> CommandResult {
     let comic_num = arguments.single::<u32>().unwrap_or(0);
-
     let latest_comic = "https://xkcd.com/info.0.json";
     let xkcd_url = format!("https://xkcd.com/{}/info.0.json", comic_num);
-
     let client = context.data.read().await.get::<ReqwestContainer>().cloned().unwrap();
-    let xkcd_request = client.get(if comic_num == 0 { latest_comic } else { &xkcd_url }).send().await?;
+    let request = client.get(if comic_num == 0 { latest_comic } else { &xkcd_url }).send().await?;
 
-    if xkcd_request.status() == StatusCode::NOT_FOUND {
+    if request.status() == StatusCode::NOT_FOUND {
         message.reply(context, "You did not provide a valid xkcd comic ID!").await?;
         return Ok(());
     }
 
-    let xkcd_response: XkcdComic = xkcd_request.json().await?;
-
-    let title = &xkcd_response.title;
-    let alt = &xkcd_response.alt;
-    let num = xkcd_response.num;
-
-    let xkcd_page = format!("https://xkcd.com/{}", num);
-    let explain_xkcd = format!("https://explainxkcd.com/wiki/index.php/{}", num);
+    let response: XkcdComic = request.json().await?;
+    let title = &response.title;
+    let alt = &response.alt;
+    let num = response.num;
+    let page = format!("https://xkcd.com/{}", num);
+    let wiki = format!("https://explainxkcd.com/wiki/index.php/{}", num);
 
     message
         .channel_id
         .send_message(context, |message| {
             message.embed(|embed| {
                 embed.title(title);
+                embed.color(0xfafafa);
                 embed.description(alt);
-                embed.image(xkcd_response.img.as_str());
-                embed.footer(|footer| footer.text(format!("xkcd comic no. {}", num)));
+                embed.image(response.img.as_str());
+                embed.footer(|f| f.text(format!("xkcd comic no. {}", &num)));
                 embed
             });
-            message.components(|c| {
-                c.create_action_row(|row| {
-                    row.create_button(|b| b.label("View Comic").style(ButtonStyle::Link).url(xkcd_page));
-                    row.create_button(|b| b.label("Explain XKCD").style(ButtonStyle::Link).url(explain_xkcd));
+            message.components(|comps| {
+                comps.create_action_row(|row| {
+                    row.create_button(|button| button.label("XKCD").style(Link).emoji(Unicode("🌐".to_string())).url(page));
+                    row.create_button(|button| button.label("Explain XKCD").style(Link).emoji(Unicode("🧠".to_string())).url(wiki));
                     row
                 });
-                c
+                comps
             });
             message
         })
