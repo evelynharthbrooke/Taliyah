5c416512c98e9e764eb6b6266d160692f2a5f148
Kamran Mackey <kamranm1200@gmail.com>
2021-06-06 14:02:20 -0600

    spotify(newreleases): removed unnecessary typehint.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/music/spotify/newreleases.rs
+++ src/commands/music/spotify/newreleases.rs
@@ -1,4 +1,4 @@
-use aspotify::{AlbumSimplified, CountryCode};
+use aspotify::CountryCode;
 use itertools::Itertools;
 
 use serenity::{
@@ -24,7 +24,7 @@ pub async fn newreleases(context: &Context, message: &Message, args: Args) -> Co
     let country_iso = CountryCode::for_alpha2_caseless(&market).unwrap();
     let country_name = locale_utils::get_country_name_from_iso(&market);
     let new_releases = spotify.browse().get_new_releases(20, 0, Some(country_iso)).await?;
-    let nr_items = new_releases.data.items.iter().map(|album: &AlbumSimplified| {
+    let nr_items = new_releases.data.items.iter().map(|album| {
         let album_name = &album.name;
         let album_artists = &album.artists.iter().map(|a| &a.name).join(", ");
         let album_date = album.release_date.unwrap().format("%B %-d, %Y").to_string();
@@ -33,7 +33,7 @@ pub async fn newreleases(context: &Context, message: &Message, args: Args) -> Co
 
     message.channel_id.send_message(context, |m| {
         m.embed(|e| {
-            e.title(format!("New Releases on Spotify for {country_name}"));
+            e.title(format!("New Releases on Spotify for: {country_name}"));
             e.colour(0x001D_B954);
             e.description(nr_items);
             e.footer(|f| f.text("Powered by the Spotify Web API."));
