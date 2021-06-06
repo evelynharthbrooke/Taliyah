7d78dc8a7da51b6505ed26be62c5a2380554cfdf
Kamran Mackey <kamranm1200@gmail.com>
2021-06-06 14:04:40 -0600

    chore: cargo fmt.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/commands/music/spotify/newreleases.rs
+++ src/commands/music/spotify/newreleases.rs
@@ -15,8 +15,11 @@ pub async fn newreleases(context: &Context, message: &Message, args: Args) -> Co
     let market = args.rest().to_string();
 
     if market.len() > 2 || market.len() < 2 {
-        message.channel_id.say(context, "You did not provide a valid market name. Name cannot be more or less than 2 characters.").await?;
-        return Ok(())
+        message
+            .channel_id
+            .say(context, "You did not provide a valid market name. Name cannot be more or less than 2 characters.")
+            .await?;
+        return Ok(());
     }
 
     let data = context.data.read().await;
@@ -24,23 +27,31 @@ pub async fn newreleases(context: &Context, message: &Message, args: Args) -> Co
     let country_iso = CountryCode::for_alpha2_caseless(&market).unwrap();
     let country_name = locale_utils::get_country_name_from_iso(&market);
     let new_releases = spotify.browse().get_new_releases(20, 0, Some(country_iso)).await?;
-    let nr_items = new_releases.data.items.iter().map(|album| {
-        let album_name = &album.name;
-        let album_artists = &album.artists.iter().map(|a| &a.name).join(", ");
-        let album_date = album.release_date.unwrap().format("%B %-d, %Y").to_string();
-        format!("**{album_name}** — {album_artists} — {album_date}")
-    }).join("\n");
+    let nr_items = new_releases
+        .data
+        .items
+        .iter()
+        .map(|album| {
+            let album_name = &album.name;
+            let album_artists = &album.artists.iter().map(|a| &a.name).join(", ");
+            let album_date = album.release_date.unwrap().format("%B %-d, %Y").to_string();
+            format!("**{album_name}** — {album_artists} — {album_date}")
+        })
+        .join("\n");
 
-    message.channel_id.send_message(context, |m| {
-        m.embed(|e| {
-            e.title(format!("New Releases on Spotify for: {country_name}"));
-            e.colour(0x001D_B954);
-            e.description(nr_items);
-            e.footer(|f| f.text("Powered by the Spotify Web API."));
-            e
-        });
-        m
-    }).await?;
+    message
+        .channel_id
+        .send_message(context, |m| {
+            m.embed(|e| {
+                e.title(format!("New Releases on Spotify for: {country_name}"));
+                e.colour(0x001D_B954);
+                e.description(nr_items);
+                e.footer(|f| f.text("Powered by the Spotify Web API."));
+                e
+            });
+            m
+        })
+        .await?;
 
     return Ok(());
 }
